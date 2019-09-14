f273e46e6818c14373e4f35adb8419cb4e8728e4
Johnny Maravelis <johnny@phoenixafterglow.com>
2019-09-14 10:58:56 -0400

    product update in db

--- bamazonDB.sql
+++ bamazonDB.sql
@@ -13,6 +13,13 @@ CREATE TABLE products(
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("DJI Mavic Pro", "electronics", 850, 18), 
-       ("Pelican Case for DJI Mavic Pro", "storage", 49.99, 120), 
-       ("GoPro Hero 7 Black", "electronics", 220, 238);
+VALUES ("BEBONCOOL RF 2.4GHz Wireless Presenter Remote Presentation USB Control Clicker", "Electronics", 14.99, 1148), 
+       ("DJI Mavic Pro", "Electronics", 850, 18),
+       ("GoPro Hero 7 Black", "Electronics", 220, 233),
+       ("Pelican Case for DJI Mavic Pro", "Storage", 49.99, 120), 
+       ("HBlife 100 ft Expandable Garden Water Hose with 8 Spray Pattern Nozzle", "Home & Garden", 34.99, 0), 
+       ("CO-Z Automatic Sliding Gate Opener Hardware Sliding Driveway Security Kit", "Home & Garden", 188.88, 5), 
+       ("CURAD Basic Care Vinyl Disposable Exam Gloves, Large (Pack of 300)", "Cleaning Supplies", 15.99, 807),  
+       ("Pelican Case for DJI Mavic Pro", "Storage", 49.99, 120), 
+       ("Pelican Case for DJI Mavic Pro", "Storage", 49.99, 120), 
+       ("Pelican Case for DJI Mavic Pro", "Storage", 49.99, 120);
