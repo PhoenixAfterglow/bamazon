9ae6ef8fd2c7594cef855a6e1723a98141af010b
Johnny Maravelis <johnny@phoenixafterglow.com>
2019-09-19 09:24:21 -0400

    Added products to DB

--- bamazonDB.sql
+++ bamazonDB.sql
@@ -17,9 +17,9 @@ VALUES ("BEBONCOOL RF 2.4GHz Wireless Presenter Remote Presentation USB Control 
        ("DJI Mavic Pro", "Electronics", 850, 18),
        ("GoPro Hero 7 Black", "Electronics", 220, 233),
        ("Pelican Case for DJI Mavic Pro", "Storage", 49.99, 120), 
+       ("SONGMICS Folding Storage Ottoman (Brown)", "Storage", 23.99, 492), 
        ("HBlife 100 ft Expandable Garden Water Hose with 8 Spray Pattern Nozzle", "Home & Garden", 34.99, 0), 
        ("CO-Z Automatic Sliding Gate Opener Hardware Sliding Driveway Security Kit", "Home & Garden", 188.88, 5), 
        ("CURAD Basic Care Vinyl Disposable Exam Gloves, Large (Pack of 300)", "Cleaning Supplies", 15.99, 807),  
-       ("Pelican Case for DJI Mavic Pro", "Storage", 49.99, 120), 
-       ("Pelican Case for DJI Mavic Pro", "Storage", 49.99, 120), 
-       ("Pelican Case for DJI Mavic Pro", "Storage", 49.99, 120);
+       ("Avery 2x4 Shipping Address Labels (100 Labels)", "Office Supplies", 4.44, 18676), 
+       ("Pentel GraphGear 500 Automatic Drafting Pencil Black (PG525A)", "Office Supplies", 4.95, 15337);
