b8929aeacc1da301e5e07313dc3969346e5bb155
Johnny Maravelis <johnny@phoenixafterglow.com>
2019-09-28 11:15:07 -0400

    dotenv set up

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -30,6 +30,16 @@ connection.connect(function(err) {
 // MySQL Connection END
 // =============================================================
 
+// Dotenv START
+require('dotenv').config()
+
+const db = require('db')
+db.connect({
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS
+})
+// Dotenv END
 
 let displayProducts = function(){
 	var query = "Select * FROM products";
